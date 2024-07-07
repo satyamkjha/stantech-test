b57989dbd7219902807b9b1643c11847b12bf134
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2024-07-07 23:30:29 +0530

    udpated functons

---
+++ netlify/functions/json-server.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = server;
